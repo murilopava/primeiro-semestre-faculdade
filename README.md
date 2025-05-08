d0c28fa21bff6ee0997a7e06806c6870a066da41
Murilo Pavanello <murilopava@gmail.com>
2025-05-08 11:10:22 -0300

    Resolução do exercício 3 do trabalho av2

--- README.md
+++
@@ -1,2 +0,0 @@
-# primeiro-semestre-faculdade
-Exercícios realizados durante o 1° semestre da faculdade de ciência da computação
--- trabalho-av2-algotimo-e-programacao/exercicio_3.cpp
+++ trabalho-av2-algotimo-e-programacao/exercicio_3.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     double valor;
-    int quantCedula;
+    int quantCedulamoeda;
 
     cout << "Indique o valor a ser decomposto: ";
     cin >> valor;
@@ -11,45 +11,86 @@ int main(){
     cout << "O valor vai receber:" << endl;
     while (valor != 0){
         if (valor >= 100){
-            quantCedula = int(valor / 100);
-            valor -= quantCedula * 100;
-            if (quantCedula == 1){
-                cout << quantCedula << " cedula de 100 " << endl;
+            quantCedulamoeda = int(valor / 100);
+            valor -= quantCedulamoeda * 100;
+
+            if (quantCedulamoeda == 1){
+                cout << quantCedulamoeda << " cedula de 100 " << endl;
             } else{
-                cout << quantCedula << " cedulas de 100 " << endl;
+                cout << quantCedulamoeda << " cedulas de 100 " << endl;
             }
         } else if (valor >= 50){
-            quantCedula = int(valor / 50);
-            valor -= quantCedula * 50;
-            if (quantCedula == 1){
-                cout << quantCedula << " cedula de 50 " << endl;
-            } else{
-                cout << quantCedula << " cedulas de 50 " << endl;
-            }
+            quantCedulamoeda = int(valor / 50);
+            valor -= quantCedulamoeda * 50;
+
+            cout << quantCedulamoeda << " cedula de 50 " << endl;         
         } else if (valor >= 20){
-            quantCedula = int(valor / 20);
-            valor -= quantCedula * 20;
-            if (quantCedula == 1){
-                cout << quantCedula << " cedula de 20 " << endl;
+            quantCedulamoeda = int(valor / 20);
+            valor -= quantCedulamoeda * 20;
+
+            if (quantCedulamoeda == 1){
+                cout << quantCedulamoeda << " cedula de 20 " << endl;
             } else{
-                cout << quantCedula << " cedulas de 20 " << endl;
+                cout << quantCedulamoeda << " cedulas de 20 " << endl;
             }
         } else if (valor >= 10){
-            quantCedula = int(valor / 10);
-            valor -= quantCedula * 10;
-            if (quantCedula == 1){
-                cout << quantCedula << " cedula de 10 " << endl;
-            } else{
-                cout << quantCedula << " cedulas de 10 " << endl;
-            }
+            quantCedulamoeda = int(valor / 10);
+            valor -= quantCedulamoeda * 10;
+
+            cout << quantCedulamoeda << " cedula de 10 " << endl;
         } else if (valor >= 5){
-            quantCedula = int(valor / 5);
-            valor -= quantCedula * 5;
-            if (quantCedula == 1){
-                cout << quantCedula << " cedula de 5 " << endl;
+            quantCedulamoeda = int(valor / 5);
+            valor -= quantCedulamoeda * 5;
+
+            if (quantCedulamoeda == 1){
+                cout << quantCedulamoeda << " cedula de 5 " << endl;
             } else{
-                cout << quantCedula << " cedulas de 5 " << endl;
+                cout << quantCedulamoeda << " cedulas de 5 " << endl;
             }
+        } else if (valor >= 1){
+            quantCedulamoeda = int(valor / 1);
+            valor -= quantCedulamoeda;
+
+            if (quantCedulamoeda == 1){
+                cout << quantCedulamoeda << " moeda de 1 " << endl;
+            } else{
+                cout << quantCedulamoeda << " moedas de 1 " << endl;
+            }
+        } else if (valor >= 0.50){
+            quantCedulamoeda = int(valor / 0.50);
+            valor -= quantCedulamoeda * 0.50;
+
+            cout << quantCedulamoeda << " moeda de 0.50 " << endl;
+        } else if (valor >= 0.25){
+            quantCedulamoeda = int(valor / 0.25);
+            valor -= quantCedulamoeda * 0.25;
+
+            cout << quantCedulamoeda << " moeda de 0.25 " << endl;
+        } else if (valor >= 0.10){
+            quantCedulamoeda = int(valor / 0.10);
+            valor -= quantCedulamoeda * 0.10;
+
+            if (quantCedulamoeda == 1){
+                cout << quantCedulamoeda << " moeda de 0.10 " << endl;
+            } else{
+                cout << quantCedulamoeda << " moedas de 0.10 " << endl;
+            } 
+        } else if (valor >= 0.05){
+            quantCedulamoeda = int(valor / 0.05);
+            valor -= quantCedulamoeda * 0.05;
+            
+            cout << quantCedulamoeda << " moeda de 0.05 " << endl;
+        } else if (valor >= 0.01){
+            quantCedulamoeda = int(valor / 0.01);
+            valor -= quantCedulamoeda * 0.01;
+
+            if (quantCedulamoeda == 0.01){
+                cout << quantCedulamoeda << " moeda de 0.01 " << endl;
+            } else{
+                cout << quantCedulamoeda << " moedas de 0.01 " << endl;
+            }
+        } else{
+            break;
         }
     }
 }
