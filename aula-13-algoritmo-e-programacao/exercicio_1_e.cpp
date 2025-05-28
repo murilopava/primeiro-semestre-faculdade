50b0a539f951a704b98cc50c07182570c230af9c
Murilo Pavanello <murilopava@gmail.com>
2025-05-28 18:15:28 -0300

    Resolução do exercicio E 1 Aula 13

--- aula-13-algoritmo-e-programacao/exercicio_1_e.cpp
+++ aula-13-algoritmo-e-programacao/exercicio_1_e.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main(){
+    int m[4][4] = {
+        {2,7,9,1},
+        {10,16,13,18},
+        {21,23,27,25},
+        {31,35,38,30}
+    };
 
+    cout << "Diagonal principal: ";
+    for(int n = 0; n < 4; n++){
+        cout << m[n][n] << " ";
+    }
 
     return 0;
 }
